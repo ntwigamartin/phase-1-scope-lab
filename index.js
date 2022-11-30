b80e32f5b772b4006b6b68457ca242fb0c5102b3
ntwigamartin <ntwiga.martin@gmail.com>
2022-11-30 15:27:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    
+}
+//console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+//console.log(setBestCustomer());
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+    
+}
+
+const leastFavoriteCustomer = 'jug';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "max"
+    return leastFavoriteCustomer
+}
+console.log(changeLeastFavoriteCustomer());
